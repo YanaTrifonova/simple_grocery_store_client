915beeb7f7cf25a86c36c6f4156eaab2d936dbe2
YanaTrifonova <trifonova.yana.prof@gmail.com>
2020-12-11 15:39:18 +0100

    added home page

--- src/pages/MyHomePage.js
+++ src/pages/MyHomePage.js
@@ -1,5 +1,20 @@
 import React from "react";
+import home from "../images/page/Home.gif";
+import "../index.css";
+import "./MyHomePage.css";
 
 export default function MyHomePage() {
-    return <h1>Home</h1>;
+    return (
+        <div className="blocks">
+            <div className="block block--left">
+                <img className="block--left-img" src={home} alt="home"/>
+            </div>
+            <div className="block block--right">
+                <div className="home-container">
+                    <h1>Welcome to Simple grocery Store</h1>
+                    <p>You could order products from the internet.</p>
+                </div>
+            </div>
+        </div>
+    )
 }
