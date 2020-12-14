058b8c14b21b3b2a18f80641978e8f9e0b6c55bb
YanaTrifonova <trifonova.yana.prof@gmail.com>
2020-12-14 09:45:06 +0100

    added page and styling for all user's order

--- src/pages/MyOrderPage.js
+++ src/pages/MyOrderPage.js
@@ -1,14 +1,16 @@
 import React, {useEffect} from "react";
-import "../App.css";
 import myOrderList from "../images/page/my_order_list.gif"
 import {useDispatch, useSelector} from "react-redux";
 import {getUserId} from "../store/order/action";
-import {getUserLoading, getUsersOrder} from "../store/order/selector";
+import {getTotalSumOfTheOrder, getUserLoading, getUsersOrder} from "../store/order/selector";
+import "../App.css";
+import "./MyOrderPage.css";
 
 export default function MyOrderPage() {
     const userId = localStorage.getItem("id");
     const orderItems = useSelector(getUsersOrder);
     const isLoading = useSelector(getUserLoading);
+    const total = useSelector(getTotalSumOfTheOrder);
 
     const dispatch = useDispatch();
 
@@ -25,9 +27,38 @@ export default function MyOrderPage() {
                 {isLoading
                  ? <em>Loading</em>
                  : <div className="home-container">
-                     {orderItems?.map((item) => {
-                         return <h1>{item.productName}</h1>
-                     })}
+                     <h1>Your order:</h1>
+                     <table className="styled-table">
+                         <thead>
+                         <tr>
+                             <th>Index</th>
+                             <th>Product name</th>
+                             <th>Price per item</th>
+                             <th>Total price</th>
+                         </tr>
+                         </thead>
+                         <tbody>
+                         {orderItems?.map((item, index) => {
+                             return (
+                                 <tr className="active-row" key={index}>
+                                     <td>{index}</td>
+                                     <td className="leftered">{item.productName}</td>
+                                     <td>{item.price}</td>
+                                     <td>{item.price * item.numberOfItems}</td>
+                                 </tr>
+                             )
+                         })}
+
+                         </tbody>
+                         <thead>
+                         <tr>
+                             <td className="total-row">Total:</td>
+                             <td>{}</td>
+                             <td>{}</td>
+                             <td className="total-row centered">{total}</td>
+                         </tr>
+                         </thead>
+                     </table>
                  </div>
                 }
             </div>
--- src/store/order/selector.js
+++ src/store/order/selector.js
@@ -5,3 +5,9 @@ export function getUsersOrder(state) {
 export function getUserLoading(state) {
     return state.myOrderPage.isLoaded
 }
+
+export function getTotalSumOfTheOrder(state) {
+    let total = 0;
+    state.myOrderPage.order.forEach((a) => total += a.numberOfItems * a.price);
+    return total;
+}
